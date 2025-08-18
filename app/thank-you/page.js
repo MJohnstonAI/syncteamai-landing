35546bb127490bb2ab0703b4d1f6a90b884e279d
MJohnstonAI <marcaj777@gmail.com>
2025-08-18 20:03:20 +0200

    Create page.js

---
+++ app/thank-you/page.js
@@ -0,0 +1,8 @@
+export default function ThankYou() {
+  return (
+    <main className="min-h-screen w-full flex flex-col items-center justify-center text-center bg-[#0a0a0a] text-white p-8">
+      <h1 className="text-4xl md:text-5xl font-extrabold mb-4">Thank You!</h1>
+      <p className="text-xl text-gray-300">You are on the list. We'll be in touch soon.</p>
+    </main>
+  );
+}
