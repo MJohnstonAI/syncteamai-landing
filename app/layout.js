55a6ae89bd619c8d08f3ac0fdf6ad5c6d536fb21
MJohnstonAI <marcaj777@gmail.com>
2025-08-18 16:52:49 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'SyncTeamAI',
+  description: 'Coming Soon: A Quantum Leap in AI Technology.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
